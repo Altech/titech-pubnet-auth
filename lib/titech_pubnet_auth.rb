640d1f9684aaa47dcd8385b62cd244e78e3db1ee
Altech <takeno.sh@gmail.com>
2013-07-29 01:33:02 +0900

    change URI for sampeling.

--- lib/titech_pubnet_auth.rb
+++ lib/titech_pubnet_auth.rb
@@ -4,33 +4,30 @@ require 'mechanize'
 require 'uri'
 require 'yaml'
 
-$:.unshift File.dirname(__FILE__)
+$:.unshift(__dir__ || File.dirname(__FILE__))
 require 'titech_pubnet_auth/bin_routines'
 require 'titech_pubnet_auth/extensions'
 
 
 class TitechPubnetAuth
-  BASE_DIR = File.expand_path('..',File.dirname(__FILE__))
-
-  SAMPLE_URI = ->{%w[www.google.co.jp www.facebook.com mixi.jp www.amazon.co.jp www.wikipedia.org www.fc2.com www.yahoo.co.jp www.kakaku.com].map{|uri| URI.parse("http://#{uri}")}[rand(3)]}
-  
+  SAMPLE_URI = URI "http://example.org"
   HTTP_PROXY = {ip: '131.112.125.238', port: 3128}
 
   def initialize(opt = {open_timeout: 3})
     @agent, @agent_with_proxy = Mechanize.new, Mechanize.new
-    [@agent,@agent_with_proxy].each{|agent|
+    [@agent, @agent_with_proxy].each{|agent|
       agent.follow_meta_refresh = true
       agent.open_timeout = opt[:open_timeout]
     }
     @agent_with_proxy.set_proxy(HTTP_PROXY[:ip], HTTP_PROXY[:port])
 
-    @private = YAML.load(File::open(File::expand_path('config/private.yml',BASE_DIR),'r'))
+    @private = YAML.load_file((__dir__ || File.dirname(__FILE__)) + '/../config/private.yml')
   end
 
   #
   # called if network_available? and not is_connected?
   #
-  def auth(sample_uri = SAMPLE_URI.call)
+  def auth(sample_uri = SAMPLE_URI)
     auth_page = @agent.get(sample_uri)
     return false if auth_page.uri.hostname != 'wlanauth.noc.titech.ac.jp'
 
@@ -43,23 +40,29 @@ class TitechPubnetAuth
 
     return is_connected?
   end
-
+  
   #
   # called if network_available?
   #
-  def is_connected?(sample_uri = SAMPLE_URI.call)
-    @agent_with_proxy.get(sample_uri).uri.hostname == sample_uri.hostname
+  def is_connected?(sample_uri = SAMPLE_URI)
+    compare_uri_by_first_part sample_uri, @agent_with_proxy.get(sample_uri).uri
   rescue # retry without the proxy
-    @agent.get(sample_uri).uri.hostname == sample_uri.hostname
+    compare_uri_by_first_part sample_uri, @agent.get(sample_uri).uri
   end
 
   #
   # note: titech-pubnet allows to access portal.titech.ac.jp without authentication.
   #
-  def network_available?(sample_uri = SAMPLE_URI.call)
+  def network_available?(sample_uri = SAMPLE_URI)
     @agent.get('http://portal.titech.ac.jp').to_b
   rescue # check another website just to make sure
     @agent.get(sample_uri).to_b rescue return false
+  end
+
+  private
+
+  def compare_uri_by_first_part uri1, uri2
+    uri1.hostname.split(".").first == uri2.hostname.split(".").first
   end
 
 end
